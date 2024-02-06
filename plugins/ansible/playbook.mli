1209ed78a79f24e8a3ff910c5312537f21721b5f
Mark Elvers <mark.elvers@tunbury.org>
2024-02-06 13:36:26 +0000

    add extra parameters

--- plugins/ansible/playbook.mli
+++ plugins/ansible/playbook.mli
@@ -9,7 +9,6 @@ val equal : t -> t -> bool
 val compare : t -> t -> int
 val pp : t Fmt.t
 val digest : t -> string
-val marshal : t -> string
 val unmarshal : string -> t
 val t_of_sexp : Sexplib0.Sexp.t -> t
 val sexp_of_t : t -> Sexplib0.Sexp.t
