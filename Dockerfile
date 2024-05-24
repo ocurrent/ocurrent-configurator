55d5885f564b7d6a2c72b46f6d531c6afebc29cc
Mark Elvers <mark.elvers@tunbury.org>
2024-05-24 11:42:29 +0100

    Push latest hosts added to master

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,7 @@ ADD --chown=opam . .
 RUN opam config exec -- dune build ./_build/install/default/bin/ocurrent-configurator
 
 FROM debian:12
-RUN apt update && apt install libsqlite3-dev libev-dev ca-certificates git netbase graphviz curl gpg -y --no-install-recommends
+RUN apt update && apt install libsqlite3-dev libev-dev ca-certificates git netbase graphviz ssh curl gpg -y --no-install-recommends
 RUN curl -Ls "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0x6125E2A8C77F2818FB7BD15B93C4A3FD7BB9C367" | gpg --dearmour -o /usr/share/keyrings/ansible-archive-keyring.gpg
 RUN echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu jammy main" | tee /etc/apt/sources.list.d/ansible.list
 RUN apt update && apt install ansible -y --no-install-recommends
--- configure-ssh.sh
+++ configure-ssh.sh
@@ -3,22 +3,46 @@
 # Generate known_hosts file
 mkdir ~/.ssh
 for host in \
+  x86-bm-c1.sw.ocaml.org \
+  x86-bm-c2.sw.ocaml.org \
+  x86-bm-c3.sw.ocaml.org \
   x86-bm-c4.sw.ocaml.org \
   x86-bm-c5.sw.ocaml.org \
   x86-bm-c6.sw.ocaml.org \
   x86-bm-c7.sw.ocaml.org \
   x86-bm-c8.sw.ocaml.org \
   x86-bm-c9.sw.ocaml.org \
+  x86-bm-c10.sw.ocaml.org \
+  x86-bm-c11.sw.ocaml.org \
+  x86-bm-c12.sw.ocaml.org \
+  x86-bm-c13.sw.ocaml.org \
+  x86-bm-c14.sw.ocaml.org \
+  x86-bm-c15.sw.ocaml.org \
+  x86-bm-c16.sw.ocaml.org \
+  x86-bm-c17.sw.ocaml.org \
+  x86-bm-c18.sw.ocaml.org \
+  x86-bm-c19.sw.ocaml.org \
+  x86-bm-c20.sw.ocaml.org \
   leafcutter.caelum.ci.dev \
   carpenter.caelum.ci.dev \
+  riscv-bm-a1.sw.ocaml.org \
+  riscv-bm-a2.sw.ocaml.org \
+  riscv-bm-a3.sw.ocaml.org \
+  riscv-bm-a4.sw.ocaml.org \
   riscv-worker-01.caelum.ci.dev \
   riscv-worker-02.caelum.ci.dev \
   riscv-worker-03.caelum.ci.dev \
+  s390x-worker-01.marist.ci.dev \
+  s390x-worker-02.marist.ci.dev \
+  c2-1.equinix.ci.dev \
   ci3.ocamllabs.io \
   ci4.ocamllabs.io \
   ci.mirage.io \
   ci.ocamllabs.io \
   deploy.ci.ocaml.org \
+  deploy.ci.dev \
+  scheduler.ci.dev \
+  images.ci.ocaml.org \
   dev1.ocamllabs.io \
   docs.ci.ocaml.org \
   staging.docs.ci.ocamllabs.io \
@@ -31,12 +55,29 @@ for host in \
   opam.ci.ocaml.org \
   ocaml.ci.dev \
   check.ci.ocaml.org \
+  registry.ci.dev \
+  molpadia.caelum.ci.dev \
+  kydoime.caelum.ci.dev \
+  ainia.caelum.ci.dev \
+  okypous.caelum.ci.dev \
+  arm64-jade-1.equinix.ci.dev \
+  arm64-jade-2.equinix.ci.dev \
+  phoebe.caelum.ci.dev \
+  clete.caelum.ci.dev \
+  toxis.caelum.ci.dev \
+  laodoke.caelum.ci.dev \
+  asteria.caelum.ci.dev \
+  doris.caelum.ci.dev \
+  iphito.caelum.ci.dev \
+  marpe.caelum.ci.dev \
+  orithia.caelum.ci.dev \
+  ocaml-multicore.ci.dev \
   147.75.84.37 ; do
   ssh-keyscan -H -t ecdsa-sha2-nistp256 $host >> ~/.ssh/known_hosts
 done
 chmod 700 ~/.ssh
 chmod 600 ~/.ssh/known_hosts
 
-# Add the deployer key
+# Add the configurator key
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGvUrmcVa69xhR2ZL8i4AyvPnFAM3fQ/Pyi5s7b0L9Lv" > ~/.ssh/id_ed25519.pub
 ln -s /run/secrets/configurator-private-key ~/.ssh/id_ed25519
